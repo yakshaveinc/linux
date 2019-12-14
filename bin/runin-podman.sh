64b2d10ac4d4f1485d7523fad9039c6d273412e5
Anatoli Babenia <anatoli@rainforce.org>
2019-12-14 17:28:10 +0300

    Add bin/runin-podman.sh

---
+++ bin/runin-podman.sh
@@ -0,0 +1,9 @@
+##!/usr/bin/env bash
+
+# name of current dir
+NAME=$(basename "$PWD")
+
+podman run -v "$(pwd)":"/root/$NAME":Z -w "/root/$NAME" -it $*
+# -t   -- allocate TTY
+# -i   -- keep STDIN open
+
