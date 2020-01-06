03c040f8860fad4bface12dd59a987d0f779fc3e
Anatoli Babenia <anatoli@rainforce.org>
2020-01-06 15:01:52 +0300

    Try automatic project sharing with LXD conainer over 9p filesystem.
    https://github.com/yakshaveinc/linux/issues/32
    
    More work needs to be done to make it work with remote containers
    by reusing LXD connection.

--- bin/lxd-runin.sh
+++ bin/lxd-runin.sh
@@ -15,7 +15,15 @@ then
 else
   echo "Creating container '$NAME' ($OSTORUN)"
   lxc launch "images:$OSTORUN" "$NAME"
-  lxc config device add "$NAME" "$NAME-shared" disk source="$PWD" path="/root/$NAME"
+  if [[ ! -e ~/go/bin/ufs ]]; then
+    echo "Mounting $PWD as /root/$NAME read-only"
+    lxc config device add "$NAME" "$NAME-shared" disk source="$PWD" path="/root/$NAME"
+  else
+    echo "Sharing writeable $PWD as /root/$NAME"
+    lxc config device add IPython shared9p proxy listen=tcp:127.0.0.1:3333 connect=tcp:127.0.0.1:3333 bind=host
+    echo "Install https://github.com/aperezdc/9pfuse and "
+    echo "run `9pfuse -D 127.0.0.1:3333 /root/$NAME` on guest."
+  fi
 fi
 
 # https://stackoverflow.com/questions/7120426/how-to-invoke-bash-run-commands-inside-the-new-shell
