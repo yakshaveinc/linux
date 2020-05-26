b451ad38483832ef2be050e673ae0a9f72299917
Anatoli Babenia <anatoli@rainforce.org>
2020-05-26 14:42:46 +0300

    `runin-podman.sh` is useful even after `toolbox` release

--- bin/runin-podman.sh
+++ bin/runin-podman.sh
@@ -7,3 +7,23 @@ podman run -v "$(pwd):/root/$NAME":Z -w "/root/$NAME" -it "$@"
 # -t   -- allocate TTY
 # -i   -- keep STDIN open
 
+# running podman/docker is not trivial and not convenient  - need to be aware
+# to :z :Z flags to volumes, -it flags to execute commands in the shell, need
+# to understand how share/unshare works if container specifies USER directive
+#
+# `runin-podman.sh` tries to address some problems with that
+#
+# [ ] you can not reentry the container if you quit
+# [x] current directory is not mounted by default (with necessary :Z flag for Fedora)
+# [x] container is not interactive be default
+# [ ] there is no default image
+#
+# all these problem are addressed by https://github.com/containers/toolbox but
+# it also add a serious security issue
+#
+# [x] `toolbox` makes the whole HOME directory r/w accessible
+#
+# that means SSH keys are readable, setup / test scripts can mess with
+# dotfiles. while the `toolbox` may be useful for OS developers, who need to
+# test package installation and rollback, it is not secure enough to run
+# random projects from GitHub in isolation
