e3ead803bf6f99b064afe2622c1c26f4f2d6e47b
Anatoli Babenia <anatoli@rainforce.org>
2020-08-01 20:56:09 +0300

    snapcraft_fedora.sh Allow to override $IMAGE with env variable

--- bin/snapcraft_fedora.sh
+++ bin/snapcraft_fedora.sh
@@ -3,6 +3,8 @@
 # print command before executing
 set -o xtrace
 
+IMAGE="${IMAGE:-yakshaveinc/snapcraft:core18}"
+
 # calling snapcraft interactively from container without installing
-podman run --rm -it -v "$PWD":/src:Z -w /src yakshaveinc/snapcraft:core18 snapcraft "$@"
+podman run --rm -it -v "$PWD":/src:Z -w /src "$IMAGE" snapcraft "$@"
 
