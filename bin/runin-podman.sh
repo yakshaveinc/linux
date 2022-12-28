7fe1b48dfad18d3b5815e10e09f3ef06991df261
Anatoli Babenia <anatoli@rainforce.org>
2022-12-28 05:09:52 +0300

    runin-podman.sh: Update documentation

--- bin/runin-podman.sh
+++ bin/runin-podman.sh
@@ -5,10 +5,18 @@
 # * Mounts current dir to `/workdir` volume (`whalebrew` convention)
 # * Containers are interactive with terminal attached `-it`
 # * Cleaned up automatically `--rm`
+# * SELinux labels are disabled
 #
 USAGE="Usage: runin-podman.sh <image> [arguments..]
 
-Set VERBOSE to any value to print script messages to stderr."
+To print script messages to stderr, set VERBOSE to any value.
+
+  VERBOSE=1 runin-podman.sh alpine
+
+To save container before exit.
+
+   podman container clone <id>
+   podman start --attach <id>"
 
 
 # Exit when any command fails
@@ -72,50 +80,52 @@ image_uid() {
   echo "$passwdline" | cut -f3 -d:
 }
 
-#### To save container before removal 
+#### What is SELinux anyway? Why it is turned off?
 #
-# podman container clone <id>
-# podman start --attach <id>
-#
-#### What is SELinux anyway?
-#
-# SELinux is a stuff that labels files on disk. Then reads labels to checks
+# SELinux is a stuff that labels files on disk. Then reads labels to check
 # if a process can access these files. :z :Z flags to volumes in
 # run_with_selinux() function are instuction to relabel files so that `podman`
 # can read/write them.
 #
-# The problem with labels is that there can be only one on a file. So if your
-# file had label, and you mounted a volume with it passing :z or :Z, you label
+# The problem with labels is that file can only have one label. So if your
+# file had label, and you mounted a volume with it passing :z or :Z, your label
 # will be lost. If you pass you home to `podman` this way, some programs may
 # stop working, and you may experience a problem logging in.
 #
 # That's why `runin-podman.sh` uses just_run() function that turns off SELunux.
 #
-#### Other things to be aware if
-#
-# If an image contains USER directive, there might be need to use share/unshare
-# commands to give container write access to volumes.
-#
 # Concerns over using :z :Z and $HOME mounts.
 # * https://github.com/kaitai-io/kaitai_struct_visualizer/pull/44/files#r543594607
 # * https://github.com/containers/podman/issues/8786
 #
-#### This script and alternatives
+
+#### Images with USER
 #
-# [ ] you can not reentry the container if you quit
-# [x] current directory is not mounted by default (with necessary :Z flag for Fedora)
-# [x] container is not interactive be default
-# [ ] there is no default image
+# When an image contains USER directive, the container won't be able to write
+# to volumes, because podman assigns that container UID and GID that are different
+# from chown/chmod attributes on filesystem. That's why such images are executed
+# with --userns=keep-id which does some magic mapping.
 #
-# all these problem are addressed by https://github.com/containers/toolbox but
-# it also adds a serious security issue
+# https://github.com/containers/podman/discussions/16258
+
+#### This script (non-)implemented features compared to alternatives
 #
-# [x] `toolbox` makes the whole HOME directory r/w accessible
+# [ ] reentry the container if you quit
+# [x] mount volumes without SELinux (which otherwise requires :Z or :z flags)
+# [x] container is interactive by default
+# [x] current dir (PWD) is a volume at /workdir
+# [ ] default image if no image is specified
+
+#### Comparison with containers/toolbox
 #
-# that means SSH keys are readable, setup / test scripts can mess with
-# dotfiles. while the `toolbox` may be useful for OS developers, who need to
+# Some script features are addressed by https://github.com/containers/toolbox but
+# it has a serious security issue that in addition to PWD, `toolbox` also makes the
+# whole HOME directory r/w accessible.
+#
+# That means SSH keys are readable, setup / test scripts can mess with
+# dotfiles. While the `toolbox` may be useful for OS developers, who need to
 # test package installation and rollback, it is not secure enough to run
-# random projects from GitHub in isolation
+# random projects from GitHub in isolation.
 
 
 IMAGE=$1
