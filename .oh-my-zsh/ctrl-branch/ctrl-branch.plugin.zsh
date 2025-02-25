02e3cd29f18a0705654997aa499c1901417db668
Anatoli Babenia <anatoli@rainforce.org>
2025-02-25 15:10:41 +0300

    ctrl-branch: Process `git branch` output line by line

--- .oh-my-zsh/ctrl-branch/ctrl-branch.plugin.zsh
+++ .oh-my-zsh/ctrl-branch/ctrl-branch.plugin.zsh
@@ -1,5 +1,17 @@
+
+# https://wiki.zshell.dev/community/zsh_handbook
+# https://gist.github.com/ClementNerma/1dd94cb0f1884b9c20d1ba0037bdcde2
+
 ctrl-branch-command() {
-  git branch -vvv
+  # read `git branch` command
+  CMD=$(git branch -vvv --color=always)
+  # split command by newline into array using f parameter expansion
+  # https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion-Flags
+  MENU=( ${(f)CMD} )
+  # traverse array and print line by line
+  for v in $MENU; do
+    print $v
+  done
 }
 
 # bind ctrl-b to type string
