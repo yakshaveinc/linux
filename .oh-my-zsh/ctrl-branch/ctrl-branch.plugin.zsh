f9357ea11759b96068ae3754bcece955ad3628dd
Anatoli Babenia <anatoli@rainforce.org>
2025-02-26 00:16:47 +0300

    ctrl-branch: Swith to branch with a hotkey (digits for now)

--- .oh-my-zsh/ctrl-branch/ctrl-branch.plugin.zsh
+++ .oh-my-zsh/ctrl-branch/ctrl-branch.plugin.zsh
@@ -8,10 +8,50 @@ ctrl-branch-command() {
   # split command by newline into array using f parameter expansion
   # https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion-Flags
   MENU=( ${(f)CMD} )
-  # traverse array and print line by line
-  for v in $MENU; do
-    print $v
+
+  local BRANCHES=()
+  local HOTKEYS=()
+  # traverse non-colored lines, detect branch and hotkey
+  CMD_NOCOLOR=$(print $CMD | sed -e 's/\x1b\[[0-9;]*m//g')
+  NCLINES=( ${(f)CMD_NOCOLOR} )
+  i=1
+  for v in $NCLINES; do
+    ## get branch name
+    # strip the column with current branch marker
+    branch=${v[3,-1]}
+    # cut after space
+    branch=${branch%% *}
+    BRANCHES+=($branch)
+
+    ## get shortcut
+    # XXX get symbol shortcut instead of numeric
+    # XXX empty labels when exhausted
+    # XXX [m] always main
+    HOTKEYS+=($i)
+    ((i++))
   done
+
+  i=1
+  for m in $MENU; do
+    print $HOTKEYS[$i] $m
+    ((i++))
+  done
+
+  # read $keypress
+  # https://zsh.sourceforge.io/Doc/Release/Shell-Builtin-Commands.html#index-read
+  read -s -k keypress
+
+  IDX=$HOTKEYS[(I)$keypress]
+  if [[ $IDX == 0 ]]; then
+    # printf "%q\n" $keypress
+    if [[ $keypress != $'\n' && $keypress != $'\033' ]]; then
+      #print "Key: $IDX #keypress"
+      printf "Undefined key: %q\n" $keypress
+    fi
+  else
+    BRANCH=$BRANCHES[$IDX]
+    git switch $BRANCH
+  fi
 }
 
 # bind ctrl-b to type string
